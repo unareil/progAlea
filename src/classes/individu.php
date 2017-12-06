0b57c7414ca81f94dab204d2a3d5005856eb8106
ANDRESebastien <andreseba_3@hotmail.com>
2017-12-06 13:36:37 +0100

    Ajout attribut itération

--- src/classes/individu.php
+++ src/classes/individu.php
@@ -1,43 +1,55 @@
 <?php
-class Individu 
-{
-    private $_firstName;
-    private $_lastName;
+    class Individu 
+    {
+        private $_firstName;
+        private $_lastName;
+        private $_iteration;
 
-    public function __construct($Lname,$Fname)
-    {
-        $this->_firstName = $Fname;
-        $this->_lastName = $Lname;
-    }
+        public function __construct($Lname,$Fname)
+        {
+            $this->_firstName = $Fname;
+            $this->_lastName = $Lname;
+            $this->_iteration = 0;
+        }
 
-    public function infosIndividu() 
-    {
-        return $this->_lastName." ".$this->_firstName;
-    }
+        public function infosIndividu() 
+        {
+            return $this->_lastName." ".$this->_firstName;
+        }
 
-    public function setNom($Lname)
-    {
-        $this->_lastName = $Lname;
-    }
+        public function setNom($Lname)
+        {
+            $this->_lastName = $Lname;
+        }
 
-    public function getNom()
-    {
-        return $this->_lastName;
-    }
+        public function getNom()
+        {
+            return $this->_lastName;
+        }
 
-    public function setPrenom($Fname)
-    {
-        $this->_firstName = $Fname;
-    }
-    
-    public function getPrenom()
-    {
-        return $this->_firstName;
-    }
+        public function setPrenom($Fname)
+        {
+            $this->_firstName = $Fname;
+        }
+        
+        public function getPrenom()
+        {
+            return $this->_firstName;
+        }
 
-    public function __destruct()
-    {
-    //    echo $this->_firstName .' '. $this->_lastName .' a ete detruit<br/>.';
+        public function augmenteIteration()
+        {
+            $this->_iteration++;
+        }
+        
+        public function getIteration()
+        {
+            return $this->_iteration;
+        }
+
+        public function __destruct()
+        {
+        //    echo $this->_firstName .' '. $this->_lastName .' a ete detruit<br/>.';
+        }
     }
-}
 ?>
